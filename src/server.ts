59534854e61a8bd1a74eb9cc9570f63f493488af
xsadia <lipearthur@hotmail.com>
2021-10-08 03:14:23 -0300

    add mongodb connection

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,14 @@
 import app from "./app";
 import { createServer } from "http";
+import { connectDB } from "./mongodb";
 
 const server = createServer(app.callback());
 
 (async () => {
+  await connectDB();
+
+  console.log("mongo connected");
+
   server.listen(process.env.PORT, () => {
     console.log(`server listening on port ${process.env.PORT}`);
   });
